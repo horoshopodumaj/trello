97ee242216e40e9c42fc0ecc5c6c557eb59c062f
Diana Murtazina <murka8831@yandex.ru>
2023-10-07 20:12:38 +0300

    added active slide

--- kanban_board/src/App.jsx
+++ kanban_board/src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Center from "./components/Center";
 import Header from "./components/Header";
+import boardsSlice from "./redux/boardSlice";
 
 function App() {
     const [isBoardModalOpen, setIsBoardModalOpen] = useState(false);
+
+    const dispatch = useDispatch();
+    const boards = useSelector((state) => state.boards);
+    const activeBoard = boards.find((board) => board.isActive);
+    if (!activeBoard && boards.length > 0) dispatch(boardsSlice.actions.setBoardActive({ index: 0 }));
+
     return (
         <div className="">
             <Header isBoardModalOpen={isBoardModalOpen} setIsBoardModalOpen={setIsBoardModalOpen} />
